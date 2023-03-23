a2c00af4a308d1938db6c7cc5cfba8203ac14cd3
dTap <imtapped@protonmail.com>
2023-03-23 14:03:21 +0700

    Added clearAllItems Button

--- src/tutorial/01-useState/starter/03-useState-array.jsx
+++ src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -4,6 +4,15 @@ import { data } from '../../../data';
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
 
+  const removeItem = () => {
+
+  };
+
+  // setPeople to an empty array can clear all items
+  const clearAllItems = () => {
+    setPeople([]);
+  };
+
   return (
     <div>
       <h2>useState array example</h2>
@@ -12,8 +21,18 @@ const UseStateArray = () => {
         const {id, name} = person;
         return <div key={id}>
           <h4>{name}</h4>
+          <button type='button'>Clear</button>
         </div>
       })}
+      {/* This button outside of the original curly braces */}
+      <button
+        type='button'
+        style={{ marginTop: '2rem' }}
+        className='btn'
+        onClick={clearAllItems}
+      >
+        clear items
+      </button>
     </div>
   );
 };
